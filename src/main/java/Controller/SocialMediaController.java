fafaf3b6563255bee8328d585f856096c4c0c2db
Melissa Perez <sweetness.index@gmail.com>
2024-11-24 06:44:11 +0000

    Add insert logic for account

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,58 +1,69 @@
 package Controller;
+
 import Model.Account;
+import Service.AccountService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
+ * TODO: You will need to write your own endpoints and handlers for your
+ * controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ * refer to prior mini-project labs and lecture materials for guidance on how a
+ * controller may be built.
  */
 public class SocialMediaController {
-    //There needs to be an AccountService
-    // AccountService accountService;
-    /*public SocialMediaController() {
-        accountService = new AccountService();
-    
-    }*/
+    AccountService accountService;
 
-     /**
-     * Method defines the structure of the Javalin Account API. Javalin methods will use handler methods
-     * to manipulate the Context object, which is a special object provided by Javalin which contains information about
-     * HTTP requests and can generate responses. There is no need to change anything in this method. 
-     
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
+    public SocialMediaController() {
+        accountService = new AccountService();
+    }
+
+    /**
+     * Method defines the structure of the Javalin Account API. Javalin methods will
+     * use handler methods
+     * to manipulate the Context object, which is a special object provided by
+     * Javalin which contains information about
+     * HTTP requests and can generate responses. There is no need to change anything
+     * in this method.
+     * 
+     * @return a Javalin app object which defines the behavior of the Javalin
+     *         controller.
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        //post endpoint on /register
+        // post endpoint on /register
         app.post("/register", this::postAccountRegistration);
 
         return app;
     }
 
-  /**
+    /**
      * Handler to register a new user.
-     * The Jackson ObjectMapper will automatically convert the JSON of the POST request into an Account object.
-     * If accountService returns a null account (meaning registering an account was unsuccessful, the API will return a 400
+     * The Jackson ObjectMapper will automatically convert the JSON of the POST
+     * request into an Account object.
+     * If accountService returns a null account (meaning registering an account was
+     * unsuccessful, the API will return a 400
      * message (client error).
-     * @param context the context object handles information HTTP requests and generates responses within Javalin. It will
-     *            be available to this method automatically thanks to the app.post method.
-     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     * 
+     * @param context the context object handles information HTTP requests and
+     *                generates responses within Javalin. It will
+     *                be available to this method automatically thanks to the
+     *                app.post method.
+     * @throws JsonProcessingException will be thrown if there is an issue
+     *                                 converting JSON into an object.
      */
-    private void postAccountRegistration(Context context) {
+    private void postAccountRegistration(Context context) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        //Account account = mapper.readValue(context.body(), Account.class);
-        /*Account registeredUser = accountService.addAccount(account);
-        if(registeredUser == null) {
+        Account account = mapper.readValue(context.body(), Account.class);
+        Account registeredUser = accountService.addAccount(account);
+        if (registeredUser == null) {
             context.status(400);
-        }
-        else {
+        } else {
             context.json(mapper.writeValueAsString(registeredUser));
-        }*/
+        }
     }
-
 
 }
--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -8,26 +8,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AccountDAO {
-     /**
+    /**
      *
      * Retrieve all accounts from the account table.
      *
      * @return all accounts.
      */
-    public List<Account> getAllAccounts(){
+    public List<Account> getAllAccounts() {
         Connection connection = ConnectionUtil.getConnection();
         List<Account> accounts = new ArrayList<>();
-        try{
+        try {
             String sql = "SELECT * FROM account";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
-            
-            while(rs.next()) {
-                Account account = new Account(rs.getInt("account_id"), rs.getString("username"), rs.getString("password"));
+
+            while (rs.next()) {
+                Account account = new Account(rs.getInt("account_id"), rs.getString("username"),
+                        rs.getString("password"));
                 accounts.add(account);
             }
 
-        }catch(SQLException exception){
+        } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
         return accounts;
@@ -38,22 +39,49 @@ public class AccountDAO {
      *
      * @param username an account username.
      */
-    public Account getAccountByUsername(String username){
+    public Account getAccountByUsername(String username) {
         Connection connection = ConnectionUtil.getConnection();
-        try{
+        try {
             String sql = "SELECT * FROM account WHERE username = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             preparedStatement.setString(1, username);
             ResultSet rs = preparedStatement.executeQuery();
-            while(rs.next()){
-                Account account = new Account(rs.getInt("account_id"), rs.getString("username"), rs.getString("password"));
+            while (rs.next()) {
+                Account account = new Account(rs.getInt("account_id"), rs.getString("username"),
+                        rs.getString("password"));
                 return account;
             }
-        }catch(SQLException exception){
+        } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
         return null;
     }
-    
+
+
+     /**
+     * Add an account record into the database which matches the values contained in the account object.
+     * 
+     * @param account an object modelling an Account. The account object does not contain an account ID.
+     */
+    public Account insertAccount(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, account.username);
+            preparedStatement.setString(2, account.password);
+            preparedStatement.executeUpdate();
+
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
